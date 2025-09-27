1b47ab6302bd2f08fa01cdc61993d644545aad1c
github <88442616+idantitydotme@users.noreply.github.com>
2025-09-27 13:04:18 -0300

    Adds nuxt-zod-i18n, @nuxtjs/device, and nuxt-echarts modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,27 @@
 export default defineNuxtConfig({
   modules: [
-    `@nuxt/ui`,
-    `@nuxtjs/i18n`,
-    `@nuxt/content`,
-    `@nuxt/image`,
-    `nuxt-og-image`,
-    `@vueuse/nuxt`,
-    `@nuxthub/core`,
-    `@nuxt/scripts`,
-    `@nuxtjs/turnstile`,
-    `@nuxtjs/sitemap`,
-    `@nuxtjs/robots`,
-    `@pinia/nuxt`,
-    `magic-regexp/nuxt`
+    "@nuxt/ui",
+    "@nuxtjs/i18n",
+    "nuxt-zod-i18n",
+    "@nuxt/content",
+    "@nuxt/image",
+    "nuxt-og-image",
+    "@vueuse/nuxt",
+    "@nuxthub/core",
+    "@nuxt/scripts",
+    "@nuxtjs/turnstile",
+    "@nuxtjs/sitemap",
+    "@nuxtjs/robots",
+    "@pinia/nuxt",
+    "magic-regexp/nuxt",
+    "nuxt-echarts",
+    "@nuxtjs/device"
   ],
   components: [
     {
-      path: `~/components`,
+      path: "~/components",
       pathPrefix: false,
-      prefix: `QK`
+      prefix: "QK"
     }
   ],
   devtools: {
@@ -26,41 +29,41 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      title: `Queer Kit`,
-      titleTemplate: `%s | Queer Kit`,
+      title: "Queer Kit",
+      titleTemplate: "%s | Queer Kit",
       meta: [
         {
-          name: `description`,
-          content: ``
+          name: "description",
+          content: ""
         },
         {
-          name: `author`,
-          content: `Queer Kit`
+          name: "author",
+          content: "Queer Kit"
         },
         {
-          name: `creator`,
-          content: `Queer Kit`
+          name: "creator",
+          content: "Queer Kit"
         }
       ],
       link: [
         {
-          rel: `icon`,
-          type: `image/svg+xml`,
-          href: `/favicon.svg`
+          rel: "icon",
+          type: "image/svg+xml",
+          href: "/favicon.svg"
         }
       ]
     }
   },
-  css: [`./app/assets/css/main.css`],
+  css: ["./app/assets/css/main.css"],
   site: {
-    url: `https://queerkit.com`,
-    name: `Queer Kit`,
+    url: "https://queerkit.com",
+    name: "Queer Kit",
     // Should be changed to true upon release to the public.
     indexable: false
   },
   colorMode: {
-    preference: `light`,
-    fallback: `light`
+    preference: "light",
+    fallback: "light"
   },
   content: {
     build: {
@@ -72,21 +75,21 @@ export default defineNuxtConfig({
     }
   },
   ui: {
-    prefix: `U`
+    prefix: "U"
   },
   runtimeConfig: {
     public: {
-      constructionPassword: process.env.SITE_PASSWORD || `secret`
+      constructionPassword: process.env.SITE_PASSWORD || "secret"
     },
     turnstile: {
       secretKey: process.env.NUXT_TURNSTILE_SECRET_KEY
     }
   },
-  compatibilityDate: `2025-07-15`,
+  compatibilityDate: "2025-07-15",
   nitro: {
     prerender: {
       crawlLinks: true,
-      routes: [`/`]
+      routes: ["/"]
     }
   },
   hub: {
@@ -113,76 +116,76 @@ export default defineNuxtConfig({
         // Extra Bold
         800
       ],
-      styles: [`normal`, `italic`]
+      styles: ["normal", "italic"]
     },
     families: [
       {
-        name: `JetBrains Mono`,
+        name: "JetBrains Mono",
         global: true,
-        provider: `local`
+        provider: "local"
       }
     ]
   },
   i18n: {
-    defaultLocale: `en`,
+    defaultLocale: "en",
     locales: [
       {
-        code: `ar`,
-        name: `العربية`
+        code: "ar",
+        name: "العربية"
       },
       {
-        code: `en`,
-        name: `English`
+        code: "en",
+        name: "English"
       },
       {
-        code: `es`,
-        name: `Español`
+        code: "es",
+        name: "Español"
       },
       {
-        code: `fr`,
-        name: `Français`
+        code: "fr",
+        name: "Français"
       },
       {
-        code: `ja`,
-        name: `日本語`
+        code: "ja",
+        name: "日本語"
       },
       {
-        code: `ko`,
-        name: `한국어`
+        code: "ko",
+        name: "한국어"
       },
       {
-        code: `pt`,
-        name: `Português`
+        code: "pt",
+        name: "Português"
       },
       {
-        code: `ro`,
-        name: `Română`
+        code: "ro",
+        name: "Română"
       },
       {
-        code: `zh_cn`,
-        name: `简体中文`
+        code: "zh_cn",
+        name: "简体中文"
       }
     ]
   },
   icon: {
-    provider: `server`,
-    class: `icon`,
-    size: `24px`,
-    mode: `css`,
+    provider: "server",
+    class: "icon",
+    size: "24px",
+    mode: "css",
     customCollections: [
       {
-        prefix: `first-party`,
-        dir: `./app/assets/icons/first-party`,
+        prefix: "first-party",
+        dir: "./app/assets/icons/first-party",
         normalizeIconName: false
       },
       {
-        prefix: `third-party`,
-        dir: `./app/assets/icons/third-party`,
+        prefix: "third-party",
+        dir: "./app/assets/icons/third-party",
         normalizeIconName: false
       },
       {
-        prefix: `flags`,
-        dir: `./app/assets/icons/flags`,
+        prefix: "flags",
+        dir: "./app/assets/icons/flags",
         normalizeIconName: false
       }
     ]
@@ -190,7 +193,7 @@ export default defineNuxtConfig({
   robots: {
     blockAiBots: false,
     blockNonSeoBots: false,
-    disallow: [`/internal`]
+    disallow: ["/internal"]
   },
   turnstile: {
     siteKey: process.env.NUXT_PUBLIC_TURNSTILE_SITE_KEY
